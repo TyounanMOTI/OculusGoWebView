982796d14e0a48b098fa7468f83c3ed170060075
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2018-06-23 15:51:22 +0900

    コントローラによるレーザーポインタを描画

---
+++ Assets/OculusGoWebView/Scripts/ControllerLazerPointer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class ControllerLazerPointer : MonoBehaviour {
+    public float maxLength = 10.0f;
+
+    LineRenderer lineRenderer;
+    Transform mainCamera;
+    Vector3[] positions = new Vector3[2];
+
+    void Start() {
+        lineRenderer = GetComponent<LineRenderer>();
+        mainCamera = Camera.main.transform;
+    }
+
+    void Update() {
+        var controller = OVRInput.GetActiveController();
+        var position = OVRInput.GetLocalControllerPosition(controller);
+        var rotation = OVRInput.GetLocalControllerRotation(controller);
+
+        var headPosition = mainCamera.position;
+        positions[0] = headPosition + position;
+        positions[1] = rotation * Vector3.forward * maxLength + positions[0];
+        lineRenderer.SetPositions(positions);
+    }
+}
